569759cdf4011249e69f5a5d5997c6a85476948c
Hugo071 <99701511+Hugo071@users.noreply.github.com>
2024-02-13 12:34:03 -0600

    Se agrega el proyecto

---
+++ ProyectoSimulado/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoSimulado.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
